e31863be10432ecd6448ceeb6001b0828bfd4129
liakwee <liakwee@gmail.com>
2012-03-19 20:35:10 +0800

    test

--- js/script.js
+++ js/script.js
@@ -3,8 +3,41 @@
 */
 
 
+$(document).ready(function() {
+	var $container = $('#container')
+	// initialize Isotope
+	$container.isotope({
+		itemSelector : '.items',
+		// options...
+		resizable: false, // disable normal resizing
+		// set columnWidth to a percentage of container width
+		masonry: { columnWidth: $container.width() / 4},
+		animationOptions: {
+			duration: 750,
+			easing: 'linear',
+			queue: false
+		}
+	});
+	
+	// update columnWidth on window resize
+	$(window).smartresize(function(){
+	  $container.isotope({
+
+			// set columnWidth to a percentage of container width
+			masonry: { columnWidth: $container.width() / 4},
+			
+	  });
+	});
+	
+	$('#filters').find('a').click(function(){
+	  	var selector = $(this).attr('data-filter');
+	  	$container.isotope({ filter: selector });
+	  	coonsole.log(selector)
+	  	return false;
+	});
+	
+
+});
 
 
-
-
-
+	
