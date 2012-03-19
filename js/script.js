e26f7aa53e761514a4f2b4cf7888a382b6d38284
liakwee <liakwee@gmail.com>
2012-03-20 00:32:22 +0800

    images

--- js/script.js
+++ js/script.js
@@ -3,41 +3,40 @@
 */
 
 
-$(document).ready(function() {
-	var $container = $('#container')
+$(function(){
+	var $container = $('#container');
+	
+	
 	// initialize Isotope
-	$container.isotope({
-		itemSelector : '.items',
-		// options...
-		resizable: false, // disable normal resizing
-		// set columnWidth to a percentage of container width
-		masonry: { columnWidth: $container.width() / 4},
-		animationOptions: {
-			duration: 750,
-			easing: 'linear',
-			queue: false
-		}
-	});
-	
-	// update columnWidth on window resize
-	$(window).smartresize(function(){
-	  $container.isotope({
-
+	$container.imagesLoaded( function(){
+		console.log($container.width()/4);
+		$container.isotope({
+			itemSelector : '.items',
+			resizable: false, // disable normal resizing
 			// set columnWidth to a percentage of container width
-			masonry: { columnWidth: $container.width() / 4},
-			
-	  });
+			masonry: { columnWidth: $container.width() / 4 }
+		});
 	});
+		
+	// update columnWidth on window resize
+    $(window).smartresize(function(){
+      $container.isotope({
+        // set columnWidth to a percentage of container width
+        masonry: {
+          columnWidth: $container.width() / 5
+        }
+      });
+    });
 	
-	$('#filters').find('a').click(function(){
-	  	var selector = $(this).attr('data-filter');
-	  	$container.isotope({ filter: selector });
-	  	coonsole.log(selector)
-	  	return false;
-	});
-	
+	// $('#filters').find('a').click(function(){
+	  	// var selector = $(this).attr('data-filter');
+	  	// $container.isotope({ filter: selector });
+	  	// coonsole.log(selector)
+	  	// return false;
+	// });
 
 });
 
 
+	
 	
